fc216eab48306c1ec1d96f8f27d12a0fc234fabc
Akshay Patil <akshaypatil00404@gmail.com>
2026-09-13 10:18:55 +0530

    feat :add data rendering,model attribute,in FirstApp

--- spring-boot-mvc/BootMVCProj01-FirstApp/src/main/java/com/akshay/controller/ShowHomeController.java
+++ spring-boot-mvc/BootMVCProj01-FirstApp/src/main/java/com/akshay/controller/ShowHomeController.java
@@ -1,5 +1,8 @@
 package com.akshay.controller;
 
+import java.time.LocalDate;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -14,6 +17,16 @@ public class ShowHomeController {
 	@RequestMapping("/")
 	public String homePage() {
 		return "welcome";
+	}
+	
+	@RequestMapping("/process")
+	public String process(Map<String,Object> map) {
+		System.out.println("ShowHomeController.process() :: " + map.getClass());
+		// add model attribute to shared memory
+		map.put("attribute", "value");
+		map.put("date", LocalDate.now());
+		
+		return "show_data";
 	}
 
 }
---
+++ spring-boot-mvc/BootMVCProj01-FirstApp/src/main/webapp/WEB-INF/pages/show_data.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ page isELIgnored="false"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Data Rendering example</title>
+</head>
+<body>
+<h2 style="color:red">from :: show_data.jsp</h2>
+<!--  use/access model attribute data-->
+${attribute},${date}
+</body>
+</html>
