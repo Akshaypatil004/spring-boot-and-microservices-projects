b202385bbfc032f3e88620a20865274ca862c2af
Akshay Patil <akshaypatil00404@gmail.com>
2026-09-17 11:51:43 +0530

    chore: initialize CRUD mini project with Spring MVC and JPA

---
+++ spring-boot-mvc/BootMVCProj08-Data-Binding-RequestParam/src/main/java/com/akshay/BootMvcProj08DataBindingRequestParamApplication.java
@@ -0,0 +1,13 @@
+package com.akshay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMvcProj08DataBindingRequestParamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMvcProj08DataBindingRequestParamApplication.class, args);
+	}
+
+}
